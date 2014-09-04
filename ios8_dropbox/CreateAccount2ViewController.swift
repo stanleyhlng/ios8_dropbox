051de594c050adfdd297d66dace2651e6bcd7e6b
stanleyn <stanleyhlng@yahoo.com.hk>
2014-09-03 23:25:09 -0700

    implement create account flow

---
+++ ios8_dropbox/CreateAccount2ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CreateAccount2ViewController.swift
+//  ios8_dropbox
+//
+//  Created by Stanley Ng on 9/3/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccount2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onBack(sender: UIButton) {
+        println("CreateAccount2ViewController - onBack")
+        dismissViewControllerAnimated(false, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ios8_dropbox/TermsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TermsViewController.swift
+//  ios8_dropbox
+//
+//  Created by Stanley Ng on 9/3/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+import UIKit
+
+class TermsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        println("TermsViewController - viewDidLoad")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onDone(sender: UIButton) {
+        println("TermsViewController - onDone")
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
